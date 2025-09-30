1ec5f8e33e8e67523483a90d56d9a16de2eb1a79
Ashenafi sileshi <ashenafisileshi7@gmail.com>
2025-09-30 04:15:15 +0300

    commit

--- front-end/my-app/src/Api/axios.js
+++ front-end/my-app/src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/clone-61fed/us-central1/api", 
+  baseURL: "http://localhost:6002/clone-61fed/us-central1/api", 
 });
 
 export {axiosInstance}
